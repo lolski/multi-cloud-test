fb8ead254848f4ca2827e6929fcc993df33804b6
Ganeshwara Hananda <ganesh@vaticle.com>
2023-04-20 14:20:32 +0100

    Move all infrastructure into the infrastructure folder

--- infrastructure/modules/deployment/modules/aws/vars.sh
+++ infrastructure/modules/deployment/modules/aws/vars.sh
@@ -2,14 +2,14 @@ export FLEET_PROJECT_ID=vaticle-typedb-cloud-test
 export FLEET_LOCATION=europe-west2
 export FLEET_CLUSTER_VERSION=1.25.5-gke.2000
 
-export AWS_VPC_ID=vpc-0a9ad0ac15ac0b87e
+export AWS_VPC_ID=vpc-05c763157d629e8d2
 export AWS_REGION=eu-west-2
-export AWS_SUBNET_IDS=subnet-0aa7067086b20b924,subnet-0edb9463e258ef660,subnet-0693175b443257058
+export AWS_SUBNET_IDS=subnet-01fa50b5a96a29ef6,subnet-0a05ea2a5850b0a76,subnet-07f4f457101ca64c3
 
 export AWS_CLUSTER_NAME=ganesh-deploy-eu-west-2-gke-aws
 
-export AWS_CONFIG_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/b5bcd61a-fc67-47c6-b3e0-52224d7e646c
-export AWS_DATABASE_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/b5bcd61a-fc67-47c6-b3e0-52224d7e646c
+export AWS_CONFIG_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/2a00fa08-9986-48ca-8e0c-0568977687d5
+export AWS_DATABASE_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/2a00fa08-9986-48ca-8e0c-0568977687d5
 
 export AWS_CP_IAM_ROLE_ARN=arn:aws:iam::098921236275:role/ganesh-deploy-eu-west-2-anthos-api-role
 export AWS_CP_IAM_INSTANCE_PROFILE=ganesh-deploy-eu-west-2-anthos-cp-instance-profile
@@ -18,8 +18,8 @@ export AWS_CP_IAM_INSTANCE_PROFILE=ganesh-deploy-eu-west-2-anthos-cp-instance-pr
 export AWS_NODE_POOL_INSTANCE_TYPE=t3.2xlarge
 export AWS_NODE_POOL_IAM_INSTANCE_PROFILE=ganesh-deploy-eu-west-2-anthos-np-instance-profile
 
-export AWS_NODE_POOL_CONFIG_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/03c9e806-3c56-4246-a952-81e31877fa85
-export AWS_NODE_POOL_ROOT_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/46d63038-0d71-47f8-a035-9f8d574398fa
+export AWS_NODE_POOL_CONFIG_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/99fbe23d-da20-4942-b083-6916b818f6d6
+export AWS_NODE_POOL_ROOT_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/0078c23b-75dc-411e-b2de-630e840bcd1f
 
 export AWS_SERVICE_ADDRESS_CIDR_BLOCKS=10.1.0.0/16
 #AWS_SERVICE_ADDRESS_CIDR_BLOCKS="10.1.0.0/16"
