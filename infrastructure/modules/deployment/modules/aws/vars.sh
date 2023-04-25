1040f34e2abbadd42c6ef477feda27891bebab6b
Ganeshwara Hananda <ganesh@vaticle.com>
2023-04-25 13:29:09 +0100

    Orchestration: scaffolding

--- infrastructure/modules/deployment/modules/aws/vars.sh
+++ infrastructure/modules/deployment/modules/aws/vars.sh
@@ -1,15 +1,15 @@
-export FLEET_PROJECT_ID=vaticle-typedb-cloud-test
+export FLEET_PROJECT_ID=vaticle-factory-prod
 export FLEET_LOCATION=europe-west2
 export FLEET_CLUSTER_VERSION=1.25.5-gke.2000
 
-export AWS_VPC_ID=vpc-05c763157d629e8d2
+export AWS_VPC_ID=vpc-0d31f47e3c1800e0d
 export AWS_REGION=eu-west-2
-export AWS_SUBNET_IDS=subnet-01fa50b5a96a29ef6,subnet-0a05ea2a5850b0a76,subnet-07f4f457101ca64c3
+export AWS_SUBNET_IDS=subnet-07d3bbfa8d914c081,subnet-01d11efb7283f8e70,subnet-0bf9f46a7f9c5a847
 
 export AWS_CLUSTER_NAME=ganesh-deploy-eu-west-2-gke-aws
 
-export AWS_CONFIG_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/2a00fa08-9986-48ca-8e0c-0568977687d5
-export AWS_DATABASE_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/2a00fa08-9986-48ca-8e0c-0568977687d5
+export AWS_CONFIG_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/649023d9-7a27-433a-bc16-8e6a681ffdbe
+export AWS_DATABASE_ENCRYPTION_KMS_KEY_ARN=arn:aws:kms:eu-west-2:098921236275:key/649023d9-7a27-433a-bc16-8e6a681ffdbe
 
 export AWS_CP_IAM_ROLE_ARN=arn:aws:iam::098921236275:role/ganesh-deploy-eu-west-2-anthos-api-role
 export AWS_CP_IAM_INSTANCE_PROFILE=ganesh-deploy-eu-west-2-anthos-cp-instance-profile
@@ -18,8 +18,8 @@ export AWS_CP_IAM_INSTANCE_PROFILE=ganesh-deploy-eu-west-2-anthos-cp-instance-pr
 export AWS_NODE_POOL_INSTANCE_TYPE=t3.2xlarge
 export AWS_NODE_POOL_IAM_INSTANCE_PROFILE=ganesh-deploy-eu-west-2-anthos-np-instance-profile
 
-export AWS_NODE_POOL_CONFIG_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/99fbe23d-da20-4942-b083-6916b818f6d6
-export AWS_NODE_POOL_ROOT_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/0078c23b-75dc-411e-b2de-630e840bcd1f
+export AWS_NODE_POOL_CONFIG_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/8706e79a-33b9-4a3a-8db1-e007ac075fe6
+export AWS_NODE_POOL_ROOT_ENCRYPTION_KEY=arn:aws:kms:eu-west-2:098921236275:key/a8099015-f254-421e-8952-79fc184befce
 
 export AWS_SERVICE_ADDRESS_CIDR_BLOCKS=10.1.0.0/16
 #AWS_SERVICE_ADDRESS_CIDR_BLOCKS="10.1.0.0/16"
---
+++ orchestration/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn deploy(cloud: &str, region: &str, name: &str) {
+    // helm client
+    // set credential
+}
